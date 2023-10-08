33936f6db2296d8b069b8d0fcfba8928c5a424be
Manuel Soria <manuel.soria@greyscaleai.com>
2023-10-08 03:22:42 -0700

    x

--- autogpts/autogpt/autogpt/prompts/default_prompts.py
+++ autogpts/autogpt/autogpt/prompts/default_prompts.py
@@ -1,44 +1,32 @@
 #########################Setup.py#################################
-
-# You will help a user find out the important, unknown aspects about a city.
-# finding unknown aspects about the city (examples include how to change currency, how to stay safe, entry restrictions/requirements)
-# - Important facts/considerations for someone visiting Argentina (from blogs)
-
-
 DEFAULT_SYSTEM_PROMPT_AICONFIG_AUTOMATIC = """
-You will build a calendar of events for someone travelling to a city.
-
-You should just consider one task: 
+You will help a user find out the important, unknown aspects about a city.
+You should just consider two tasks:
+- finding unknown aspects about the city (examples include how to change currency, how to stay safe, entry restrictions/requirements)
 - finding interesting venues in the provided dates
-
 The user will provide the task, you will provide only the output in the exact format specified below with no explanation or conversation.
 If you don't know the trip's dates, you should ask.
 Example input:
 Help me with going to Buenos Aires
-
 Example output:
 Name: TravelGPT
 Description: a tourist agent that helps users find information on their trips
 Goals:
 - Understand and solve user's concerns / interests in the trip
+- Important facts/considerations for someone visiting Argentina (from blogs)
 - Interesting venues to visit in Buenos Aires during the trip's dates
 - Provide custom assistance based on files provided by the user.
 - Always start by reading trip_details.pdf
 """
-
 # - Interesting venues to visit
 # - Interesting activites to do
 # - What to pack for the trip
-
 # - Budget, dates and other details (should ask user)
-
 # - Activities available during the selected dates in Buenos Aires
-
 DEFAULT_TASK_PROMPT_AICONFIG_AUTOMATIC = (
     "Task: '{{user_prompt}}'\n"
     "Respond only with the output in the exact format specified in the system prompt, with no explanation or conversation.\n"
     "Always start by reading trip_details.pdf and any other file in dir."
     "MAKE SURE you include all of the keys in your output AS SPECIFIED in the system prompt."
 )
-
 DEFAULT_USER_DESIRE_PROMPT = "Write a wikipedia style article about the project: https://github.com/significant-gravitas/AutoGPT"  # Default prompt
--- autogpts/autogpt/autogpt/prompts/prompt.py
+++ autogpts/autogpt/autogpt/prompts/prompt.py
@@ -1,6 +1,6 @@
 DEFAULT_TRIGGERING_PROMPT = (
     """
-    Determine exactly one command to use next based on the given goals 
+    Determine exactly one command to use next based on the given goals
     and the progress you have made so far,
     and respond using the JSON schema specified previously.
     If doing a web search, you should strive to make it as directed as possible. Here are some guidelines to get the best results:
@@ -14,6 +14,5 @@ DEFAULT_TRIGGERING_PROMPT = (
     user provided documents or the web:
     """
 )
-
 # If you previously made a google search and the selected webpage did not return interesting information,
 # you can always try doing another fetch for one of the others
